353ee90b8b12ca6f340a2e9bbe441a58fe1eeab3
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2022-12-22 11:40:10 +0000

    Play constructor

--- src/commands/Play.js
+++ src/commands/Play.js
@@ -11,8 +11,8 @@ module.exports = {
 				.setDescription('URL ou nome')
 				.setRequired(true)),
 	async execute(interaction) {
-		if (!interaction.member.voice.channelId) return await interaction.reply({ content: 'You are not in a voice channel!', ephemeral: true });
-		if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) return await interaction.reply({ content: 'You are not in my voice channel!', ephemeral: true });
+		if (!interaction.member.voice.channelId) return await interaction.reply({ content: 'Você não está em um canal de voz!', ephemeral: true });
+		if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) return await interaction.reply({ content: 'Eu já estou em um canal de voz!', ephemeral: true });
 		const query = interaction.options.getString('query');
 		const queue = player.createQueue(interaction.guild, {
 			metadata: {
@@ -20,23 +20,23 @@ module.exports = {
 			},
 		});
 
-		// verify vc connection
+		// Verificar conexão da voz
 		try {
 			if (!queue.connection) await queue.connect(interaction.member.voice.channel);
 		}
 		catch {
 			queue.destroy();
-			return await interaction.reply({ content: 'Could not join your voice channel!', ephemeral: true });
+			return await interaction.reply({ content: 'Não foi possível conectar no seu canal de voz!', ephemeral: true });
 		}
 
 		await interaction.deferReply();
 		const track = await player.search(query, {
 			requestedBy: interaction.user,
 		}).then(x => x.tracks[0]);
-		if (!track) return await interaction.followUp({ content: `❌ | Track **${query}** not found!` });
+		if (!track) return await interaction.followUp({ content: `❌ | Música **${query}** não encontrada!` });
 
 		queue.play(track);
 
-		return await interaction.followUp({ content: `⏱️ | Loading track **${track.title}**!` });
+		return await interaction.followUp({ content: `⏱️ | Carregando música **${track.title}**!` });
 	},
 };
--- src/config/Player.js
+++ src/config/Player.js
@@ -2,4 +2,8 @@ const { discordClient } = require('./DiscordBot.js');
 const { Player } = require('discord-player');
 const player = new Player(discordClient);
 player.on('trackStart', (queue, track) => queue.metadata.channel.send(`🎶 | Now playing **${track.title}**!`));
+
+player.on('error', (queue, error) => {
+	console.log(error);
+});
 module.exports.player = player;
