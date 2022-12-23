1f575cb004aa2c2f213b543a0a57e490d45226b4
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2022-12-23 10:33:40 -0300

    ARRUMANDO A PORTA

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv/config');
 const express = require('express');
+const { discordClient } = require('./src/config/DiscordBot.js');
+require('./src/config/Player.js');
+require('./src/config/Event.js');
+require('./src/config/Commands.js');
 const app = express();
 app.get('/', (req, res) => {
 	return res.send('Online!');
 });
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	discordClient.login(process.env.TOKEN);
 	console.log('Online!');
 });
-const { discordClient } = require('./src/config/DiscordBot.js');
-require('./src/config/Player.js');
-require('./src/config/Event.js');
-require('./src/config/Commands.js');
 
-discordClient.login(process.env.TOKEN);
