cbcfe485419c21341fd1c8d8675385867c12c125
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2022-12-23 10:00:52 -0300

    VERCEL

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 require('dotenv/config');
+const { express } = require('express');
+const app = express();
+app.get('/', (req, res) => {
+	return res.send('Online!');
+});
+
+app.listen(5000, () => {
+	console.log('Online!');
+});
 const { discordClient } = require('./src/config/DiscordBot.js');
 require('./src/config/Player.js');
 require('./src/config/Event.js');
